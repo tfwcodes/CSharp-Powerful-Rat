948d1f692caabbdc3af0ae6ef1ef5b83608f6c92
tfwcodes <86290437+tfwcodes@users.noreply.github.com>
2026-07-16 01:23:39 +0300

    Update Protocol.cs

--- Server/Protocol.cs
+++ Server/Protocol.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Encryption;
 
@@ -9,99 +9,94 @@ namespace Protocol
 {
     public static class Protocol
     {
+
+        private static readonly ConditionalWeakTable<Stream, Encryption.Encryption> _sessions
+            = new ConditionalWeakTable<Stream, Encryption.Encryption>();
+
+
         public static void ReadAll(this Stream stream, byte[] buffer)
         {
-            if (buffer.Length == 0)
-            {
-                return;
-            }
+            if (buffer.Length == 0) return;
 
             var total = 0;
             while (total != buffer.Length)
             {
-                
                 var read = stream.Read(buffer, total, buffer.Length - total);
-                if (read == 0)
-                {
-                    throw new Exception("Connection Closed");
-                }
+                if (read == 0) throw new Exception("Connection Closed");
                 total += read;
             }
         }
 
-        /// <summary>
-        /// Read the size of the message.
-        /// </summary>
-        /// <param name="stream"></param>
         public static int ReadSize(this Stream stream)
         {
             var sizeMsg = new byte[4];
-
             stream.ReadAll(sizeMsg);
-
             return BitConverter.ToInt32(sizeMsg, 0);
         }
 
-
-
-        /// <summary>
-        /// Read the bytes of the message.
-        /// </summary>
-        /// <param name="stream"></param>
-        /// <returns></returns>
         public static byte[] ReadBytes(this Stream stream)
-        {  
+        {
             var size = ReadSize(stream);
-            var bytesMsg = new byte[size];
-
-            stream.ReadAll(bytesMsg);
-
-            return bytesMsg;
+            var buf = new byte[size];
+            stream.ReadAll(buf);
+            return buf;
         }
-
-        /// <summary>
-        /// Send the bytes trough the network.
-        /// </summary>
-        /// <param name="stream"></param>
-        /// <param name="buffer"></param>
+]
         public static void WriteBytes(this Stream stream, byte[] buffer)
-        {   
-            int length = buffer.Length;
-
-            stream.Write(BitConverter.GetBytes(length), 0, 4);
+        {
+            stream.Write(BitConverter.GetBytes(buffer.Length), 0, 4);
             stream.Write(buffer, 0, buffer.Length);
         }
 
 
         /// <summary>
-        /// Send the message.
+        /// Does the ECDH handshake on this stream. The server sends the public key, and waits for the client's one. (the client doing the same)
         /// </summary>
-        /// <param name="stream"></param>
-        /// <param name="message"></param>
-        public static void WriteString(this Stream stream, string message)
+        public static void InitializeEncryption(this Stream stream, bool isServer)
         {
-            var aes = new Encryption.Encryption(message);
-            var encrypted = aes.Encrypt(message);
-            var key = aes._key;
-            stream.WriteBytes(key);
+            var enc = new Encryption.Encryption();
 
-            stream.WriteBytes(Encoding.UTF8.GetBytes(encrypted));
+            if (isServer)
+            {
+                // the server
+                stream.WriteBytes(enc.GetPublicKey());
+                var clientPubKey = stream.ReadBytes();
+                enc.DeriveSharedKey(clientPubKey);
+            }
+            else
+            {
+                // the client
+                var serverPubKey = stream.ReadBytes();
+                stream.WriteBytes(enc.GetPublicKey());
+                enc.DeriveSharedKey(serverPubKey);
+            }
+
+            _sessions.Add(stream, enc);
         }
 
 
         /// <summary>
-        /// Receive the message.
+        /// Write a string.
         /// </summary>
-        /// <param name="stream"></param>
-        /// <returns></returns>
+        public static void WriteString(this Stream stream, string message)
+        {
+            if (!_sessions.TryGetValue(stream, out var enc))
+                throw new InvalidOperationException("Apeleaza InitializeEncryption() inainte de WriteString.");
+
+            var encrypted = enc.Encrypt(message);
+            stream.WriteBytes(Encoding.UTF8.GetBytes(encrypted));
+        }
+
+        /// <summary>
+        /// Reads and decrypts.
+        /// </summary>
         public static string ReadString(this Stream stream)
         {
-            var key = stream.ReadBytes();
-            var decryption = new Decryption();
+            if (!_sessions.TryGetValue(stream, out var enc))
+                throw new InvalidOperationException("Apeleaza InitializeEncryption() inainte de ReadString.");
 
-            var decryptionText = Encoding.UTF8.GetString(stream.ReadBytes());
-            return decryption.Decrypt(decryptionText, key);
+            var cipherText = Encoding.UTF8.GetString(stream.ReadBytes());
+            return enc.Decrypt(cipherText);
         }
     }
 }
-    
